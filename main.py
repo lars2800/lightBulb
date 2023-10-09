5c337d6f690d351097df1510b1bbbe19e3973f5f
lars2800 <92262180+lars2800@users.noreply.github.com>
2023-10-09 18:28:12 +0200

    Added coments via chatgpt

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
+# Import necessary libraries and modules
 import pygame as pg
 import numpy as np
 from OpenGL.GL import *
 from OpenGL.GL.shaders import *
 import ctypes
 
+# Define the version of the application
 __version__ = "DEV 0.0.0"
 
+# Create an Engine class to manage the main application logic
 class Engine:
     def __init__(self) -> None:
         """
-        
-        Main engineClass of Lightbulb
+        Initialize the Engine class.
 
+        This is the main class for the Lightbulb application.
         """
-        self.windSize  = (800,600)
+        # Define initial window properties
+        self.windSize = (800, 600)
         self.windTitle = "Lightbulb " + __version__
-        self.clearColor = (0.1 ,0.2 ,0.2 ,  1.0)
-        self.maxFps = 120 # 0 = unlimeted
+        self.clearColor = (0.1, 0.2, 0.2, 1.0)
+        self.maxFps = 120  # Maximum frames per second (0 for unlimited)
 
+        # Initialize pygame and set OpenGL context attributes
         pg.init()
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION,3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION,3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,pg.GL_CONTEXT_PROFILE_CORE)
-        self.display = pg.display.set_mode(self.windSize, pg.OPENGL| pg.DOUBLEBUF | pg.RESIZABLE)
-        
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        self.display = pg.display.set_mode(self.windSize, pg.OPENGL | pg.DOUBLEBUF | pg.RESIZABLE)
+
+        # Create a pygame clock for controlling frame rate
         self.pgClock = pg.time.Clock()
 
-        self.shaderProgram = self.compileShaders("shader.vert","shader.frag")
+        # Compile shaders and set up OpenGL settings
+        self.shaderProgram = self.compileShaders("shader.vert", "shader.frag")
         glUseProgram(self.shaderProgram)
         glViewport(0, 0, *self.windSize)
         glClearColor(*self.clearColor)
 
+        # Create the scene
         self.scene = Mesh()
 
+        # Start the main loop
         self.run()
 
-    def compileShaders(self,vertShaderPath,fragShaderPath):
+    # Function to compile vertex and fragment shaders into a shader program
+    def compileShaders(self, vertShaderPath, fragShaderPath):
         with open(vertShaderPath) as vertShaderFile:
-            vertShader = compileShader(vertShaderFile.read(),GL_VERTEX_SHADER)
+            vertShader = compileShader(vertShaderFile.read(), GL_VERTEX_SHADER)
 
         with open(fragShaderPath) as fragShaderFile:
-            fragShader = compileShader(fragShaderFile.read(),GL_FRAGMENT_SHADER)
+            fragShader = compileShader(fragShaderFile.read(), GL_FRAGMENT_SHADER)
 
-        shaderProgram = compileProgram(vertShader,fragShader)
+        shaderProgram = compileProgram(vertShader, fragShader)
         glDeleteShader(vertShader)
         glDeleteShader(fragShader)
         return shaderProgram
 
+    # Function to render the scene
     def render(self):
         glUseProgram(self.shaderProgram)
         glClear(GL_COLOR_BUFFER_BIT)
@@ -54,57 +65,59 @@ class Engine:
         self.scene.render()
 
         pg.display.flip()
-    
+
+    # Function to handle pygame events
     def pollEvents(self):
-
-        for event in pg.event.get(): # loop over all events
-
+        for event in pg.event.get():  # Loop over all events
             if event.type == pg.QUIT:
-                self.running = False # stop running
-            
-            if event.type == pg.VIDEORESIZE: # Window resize
-                self.windSize = event.dict['size'] # new size
-                glViewport(0,0, *self.windSize)    # Pass to OpenGL
-                print(self.windSize)
+                self.running = False  # Stop running
 
+            if event.type == pg.VIDEORESIZE:  # Window resize
+                self.windSize = event.dict['size']  # New size
+                glViewport(0, 0, *self.windSize)  # Pass to OpenGL
+
+    # Main loop function
     def run(self):
         self.running = True
 
         while self.running:
-            self.pollEvents()    
+            self.pollEvents()
             self.render()
 
             self.pgClock.tick(self.maxFps)
-            self.fps = round(self.pgClock.get_fps(),1)
-            pg.display.set_caption(self.windTitle+" fps: "+str(self.fps))
-        
-        self.terminate()
-    
+            self.fps = round(self.pgClock.get_fps(), 1)
+            pg.display.set_caption(self.windTitle + " fps: " + str(self.fps))
 
+        self.terminate()
+
+    # Function to clean up and terminate the application
     def terminate(self):
         self.scene.terminate()
         glDeleteProgram(self.shaderProgram)
         pg.quit()
 
+# Create a Mesh class to manage geometry and rendering
 class Mesh:
     def __init__(self) -> None:
-        
+        # Define vertex data and indices for a simple quad
         self.vertices = np.array([
-             0.5,  0.5, 0.0,  # top right
-             0.5, -0.5, 0.0,  # bottom right
+            0.5, 0.5, 0.0,  # top right
+            0.5, -0.5, 0.0,  # bottom right
             -0.5, -0.5, 0.0,  # bottom left
-            -0.5,  0.5, 0.0   # top left 
-        ],np.float32)
-         
-        self.indices = np.array([
-            0, 1, 3,  # first Triangle
-            1, 2, 3   # second Triangle
-        ],np.int32)
+            -0.5, 0.5, 0.0  # top left
+        ], np.float32)
 
+        self.indices = np.array([
+            0, 1, 3,  # First Triangle
+            1, 2, 3   # Second Triangle
+        ], np.int32)
+
+        # Generate OpenGL objects for the mesh
         self.VAO = glGenVertexArrays(1)
         self.VBO = glGenBuffers(1)
         self.EBO = glGenBuffers(1)
-        # bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
+        # Bind the Vertex Array Object, Vertex Buffer, and Element Buffer
         glBindVertexArray(self.VAO)
 
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
@@ -116,29 +129,20 @@ class Mesh:
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * 4, ctypes.c_void_p(0))
         glEnableVertexAttribArray(0)
 
-        # note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        # Unbind VBO and VAO
         glBindBuffer(GL_ARRAY_BUFFER, 0)
-
-        # remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-        #glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-        # You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-        # VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
         glBindVertexArray(0)
 
-        # uncomment this call to draw in wireframe polygons.
-        #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    
+    # Function to render the mesh
     def render(self):
-        glBindVertexArray(self.VAO); # seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        #glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(self.VAO)
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
-        # glBindVertexArray(0); // no need to unbind it every time 
-    
+
+    # Function to clean up and delete OpenGL objects
     def terminate(self):
         glDeleteVertexArrays(1, (self.VAO,))
         glDeleteBuffers(1, (self.VBO,))
 
-
 if __name__ == "__main__":
+    # Create an instance of the Engine class and start the application
     demoEngine = Engine()
